33b0f2761c7bee99f23d34ac31ad321c08c60090
holalluis <holalluis@gmail.com>
2019-02-23 20:43:12 +0100

    update

--- blog/index.php
+++ blog/index.php
@@ -2,6 +2,7 @@
   <meta charset=utf-8>
   <link rel="stylesheet" href="../css.css">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" href="../img/favicon.png" type="image/x-icon">
   <title>lluis.ovh</title>
   <style>
     body {
