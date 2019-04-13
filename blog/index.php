36e650c8d08c99aef27aaac70ffaadd328b1d3a6
holalluis <holalluis@gmail.com>
2019-04-13 04:09:48 +0200

    update

--- blog/index.php
+++ blog/index.php
@@ -4,34 +4,23 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="icon" href="../img/favicon.png" type="image/x-icon">
   <title>lluis.ovh</title>
-  <style>
-    body {
-      font-size:16px;
-    }
-  </style>
 </head><body>
 <main>
-
 <h2 style=text-align:center><a href=../>Inici</a> / Blog</h2>
-
 <ul>
 <?php
   include '../timeAgo.php';
-
   $dir = getcwd();
   $files_in_dir=array_diff(scandir($dir),array('..', '.'));
   rsort($files_in_dir);
   foreach($files_in_dir as $file){
-
     if($file=='index.php')continue;
     if($file[0]=='.')continue;
-
     $data = substr($file,0,10);
     $ago =timeAgo($data);
-
     $nom = str_replace('.html','',$file);
     $nom = str_replace('_-_',' - ',$nom);
-
+    $nom = preg_replace('/([a-z])-([a-z])/i','$1 $2',$nom);
     echo "<li><a href='$file'>$nom</a> ($ago)";
   }
 ?>
--- index.php
+++ index.php
@@ -4,22 +4,16 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="icon" href="img/favicon.png" type="image/x-icon">
   <title>lluis.ovh</title>
-
   <style>
     main{
       display:grid;
       grid-template-columns:62% 38%;
       grid-gap:10px;
     }
-
-    main > div{
-    }
   </style>
 </head><body>
-
 <h1 style=text-align:center>lluis.ovh</h1>
 
-<!--main-->
 <main>
   <!--welcome-->
   <div>
@@ -45,8 +39,7 @@
     <ul>
       <?php
         include 'timeAgo.php'; //function
-
-        //mostra 10 entrades blog
+        //mostra 10 últimes entrades blog
         $dir = getcwd()."/blog";
         $files_in_dir=array_diff(scandir($dir),array('..', '.'));
         rsort($files_in_dir);
@@ -54,16 +47,14 @@
         foreach($files_in_dir as $file){
           if($file=='index.php')continue;
           if($file[0]=='.')continue;
-
-          $date = substr($file,0,10); //string de 10 caràcters
-          $timeago = timeAgo($date);  //string
-          $nom = substr($file,13); //nom article
+          $date = substr($file,0,10); //string de 10 caràcters (data)
+          $timeago = timeAgo($date);  //string ("fa 2 mesos")
+          $nom = substr($file,13);    //titol article comença a la posició 13
           $nom = str_replace('_',' ',$nom);
           $nom = str_replace('.html','',$nom);
           $nom = preg_replace('/(\w)-(\w)/i','$1 $2',$nom);
           $nom = ucfirst($nom);
-
-          echo "<li><small><a href='blog/$file'>$nom</a> ($timeago)</small>";
+          echo "<li><a href='blog/$file'>$nom</a> ($timeago)</li>";
           $i++;
           if($i>10)break;
         }
@@ -74,37 +65,35 @@
   <!--setup-->
   <div>
     <h3>Setup</h3>
-
-      <p><i>"Let's use the terminal more, because if you don't use it, your brain gets
-      sloppy, and you don't want a sloppy brain".</i></p>
-
+    <p><i>"Let's use the terminal more, because if you don't use it, your brain gets
+    sloppy, and you don't want a sloppy brain".</i></p>
     <ul>
-      <li>Sistem operatiu:          <a href="https://www.gnu.org/">GNU Linux</a>
-                                    <a href=//debian.org                        target=_blank>debian</a> (amb
-                                    <a href="//i3wm.org"                        target=_blank>i3</a> /
-                                    <a href="//gnome.org"                       target=_blank>gnome</a>).
-      <li>Terminal manager:         <a href="//tmux.github.io"                  target=_blank>tmux</a>.
-      <li>Editor de text:           <a href="//www.vim.org/"                    target=_blank>vim</a> /
-                                    <a href="//neovim.io/"                      target=_blank>neovim</a>.
-      <li>Control de versions:      <a href="//git-scm.org"                     target=_blank>git</a>.
-      <li>Shell:                    <a href="//gnu.org/software/bash/bash.html" target=_blank>bash</a> /
-                                    <a href="//https://fishshell.com/"          target=_blank>fish</a> /
-                                    <a href="//zsh.sourceforge.net/"            target=_blank>zsh</a>.
-      <li>Accés remot:              <a href="//ssh.com/ssh"                     target=_blank>ssh</a>.
-      <li>Agenda:                   <a href="//calcurse.org"                    target=_blank>calcurse + caldav</a>.
-      <li>Explorador:               <a href="//ranger.github.io"                target=_blank>ranger</a> /
-                                    <a href="//vifm.info/"                      target=_blank>vifm</a>.
-      <li>Email:                    <a href="//neomutt.org"                     target=_blank>neomutt</a>.
-      <li>Fulla de càlcul:          <a href="//github.com/andmarti1424/sc-im"   target=_blank>scim</a>.
-      <li>Elaboració de documents:  <a href="//gnu.org/software/groff/"         target=_blank>groff + eqn + tbl</a>.
-      <li>Visor documents pdf:      <a href="//pwmt.org/projects/zathura/"      target=_blank>zathura</a>.
-      <li>Reproductor multimèdia:   <a href="//mpv.io"                          target=_blank>mpv</a> /
-                                    <a href="//videolan.org"                    target=_blank>vlc</a>.
-      <li>Visor imatges:            <a href="https://github.com/muennich/sxiv"  target=_blank>sxiv</a>.
+      <li>Sistem operatiu:         <a href="https://www.gnu.org/"              target=_blank>GNU Linux</a> /
+                                   <a href=//debian.org                        target=_blank>debian</a> (amb
+                                   <a href="//i3wm.org"                        target=_blank>i3</a> /
+                                   <a href="//gnome.org"                       target=_blank>gnome</a>).
+      <li>Terminal manager:        <a href="//tmux.github.io"                  target=_blank>tmux</a>.
+      <li>Editor de text:          <a href="//www.vim.org/"                    target=_blank>vim</a> /
+                                   <a href="//neovim.io/"                      target=_blank>neovim</a>.
+      <li>Control de versions:     <a href="//git-scm.org"                     target=_blank>git</a>.
+      <li>Shell:                   <a href="//gnu.org/software/bash/bash.html" target=_blank>bash</a> /
+                                   <a href="//https://fishshell.com/"          target=_blank>fish</a> /
+                                   <a href="//zsh.sourceforge.net/"            target=_blank>zsh</a>.
+      <li>Accés remot:             <a href="//ssh.com/ssh"                     target=_blank>ssh</a>.
+      <li>Agenda:                  <a href="//calcurse.org"                    target=_blank>calcurse + caldav</a>.
+      <li>Explorador:              <a href="//ranger.github.io"                target=_blank>ranger</a> /
+                                   <a href="//vifm.info/"                      target=_blank>vifm</a>.
+      <li>Email:                   <a href="//neomutt.org"                     target=_blank>neomutt</a>.
+      <li>Fulla de càlcul:         <a href="//github.com/andmarti1424/sc-im"   target=_blank>scim</a>.
+      <li>Elaboració de documents: <a href="//gnu.org/software/groff/"         target=_blank>groff + eqn + tbl</a>.
+      <li>Visor documents pdf:     <a href="//pwmt.org/projects/zathura/"      target=_blank>zathura</a>.
+      <li>Reproductor multimèdia:  <a href="//mpv.io"                          target=_blank>mpv</a> /
+                                   <a href="//videolan.org"                    target=_blank>vlc</a>.
+      <li>Visor imatges:           <a href="https://github.com/muennich/sxiv"  target=_blank>sxiv</a>.
     </ul>
   </div>
 
-  <!--info-->
+  <!--seccions-->
   <div>
     <h3>Seccions</h3>
     <ul>
