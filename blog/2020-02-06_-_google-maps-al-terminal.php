bcdec916062d6848f10e09fb3b71759b9332e43a
holalluis <holalluis@gmail.com>
2020-02-06 12:27:45 +0100

    update

---
+++ blog/2020-02-06_-_google-maps-al-terminal.php
@@ -0,0 +1,21 @@
+<?php include('css.php')?>
+<link rel="stylesheet" type="text/css" href="asciinema-player/asciinema-player.css">
+<h1>google maps al terminal</h1><hr>
+<div><code>2020-02-06</code><div><hr><article>
+
+La comanda següent és espectacular:
+
+<pre class=prettyprint><code>
+  $ telnet mapscii.me
+</code>
+</pre>
+
+És més fàcil veure-ho en acció que explicar-ho:
+
+<br><br>
+<asciinema-player src="asciinema-player/mapscii.cast"></asciinema-player>
+<script src="asciinema-player/asciinema-player.js"></script>
+
+<br><br>
+
+<!--fi--><p>Salut!</p><p>Lluís</p></article><hr><a href=index.php>Blog</a>
