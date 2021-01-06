22d38a1f103289e4cdcfc663b4ca97e0c282de2f
Lluís Bosch <holalluis@gmail.com>
2021-01-06 22:42:07 +0100

    update

--- blog/2019-02-22_-_les-meves-webs.php
+++ blog/2019-02-22_-_les-meves-webs.php
@@ -7,13 +7,13 @@
   exercici.  Avui he estat recopilant i algunes d'aquestes webs. Algunes de les
   més destacades són:</p>
   <ul>
-    <li><a target=_blank href=//lluis.ovh/acudits >Acudits aleatoris</a> (la meva primera web)
-    <li><a target=_blank href=//butionline.cat>Buti online </a> (joc de la botifarra online)
+    <li><a target=_blank href=//lluis.ovh/acudits>Acudits aleatoris</a> (la meva primera web)
+    <li><a target=_blank href=//lluis.ovh/butionline>Buti online </a> (joc de la botifarra online)
     <li><a target=_blank href=//lluis.ovh/quantsDiesTens>Quants dies tens</a> (calculadora de dies viscuts)
     <li><a target=_blank href=//lluis.ovh/jkl>jkl</a> (mini joc per entrenar l'agilitat mental)
     <li><a target=_blank href=//lluis.ovh/notesMusicals>Notes musicals</a> (mini joc per aprendre a llegir partitures)
     <li><a target=_blank href=//lluis.ovh/dni>Conversor dni</a> (converteix el teu dni a una paraula)
-    <li><a target=_blank href=//consulta.ovh>Consulta</a> (consulta sobre la monarquia espanyola)
+    <li><a target=_blank href=//lluis.ovh/consulta>Consulta</a> (consulta sobre la monarquia espanyola)
   </ul>
   <p>Aquí pots trobar <a href=//lluis.ovh>la resta de webs</a>.</p>
   <p>Salut!</p>
