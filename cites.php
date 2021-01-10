29f4cba64eb3a084f05a7e279448b1178d3cbad1
Lluís Bosch <holalluis@gmail.com>
2021-01-10 14:09:16 +0100

    update

--- cites.php
+++ cites.php
@@ -1,4 +1,10 @@
 <?php include'header.php'?>
+<style>
+  li{
+    font-style:italic;
+  }
+</style>
+
 <b>Cites random trobades per internet, rescatades aquí</b>
 <ul>
   <li>
@@ -11,8 +17,44 @@
     reproduction. I spend most of my days in a text browser, text editor and
     text terminal, looking at barely moving letters.
   </li>
+  <li>
+    The only true wisdom is knowing you know nothing
+  </li>
+  <li>
+    8 tips:
+    <ol>
+      <li>Figure out what makes you happy
+      <li>Focus on what you can control
+      <li>Automate everything
+      <li>Stop consuming; start creating
+      <li>Throw your TV out the window
+      <li>Travel to other countries
+      <li>Experiment
+      <li>Try before you buy. Rent instead of own. Borrow when possible and purchase used when not.
+    </ol>
+  </li>
+  <li>
+    Holstee manifesto (2009):
+    <ul>
+      <li>This is your life
+      <li>Do what you love and do it often.
+      <li>If you don't like something, change it.
+      <li>If you don't like your job, quit.
+      <li>If you don't have enough time, stop watching tv.
+      <li>If you are looking for the love of your life, stop; they will be waiting for you when you start doing thins you love.
+      <li>Stop over analyzing, life is simple.
+      <li>All emotions are beautiful, when you eat, appreciate every last bite.
+      <li>Open your mind, arms, and heart to new things and people, we are united in our differences.
+      <li>Ask the next person you see what their passion is, and share your inspiring dream to them.
+      <li>Travel often; getting lost will help you find yourself.
+      <li>Some opportunities only come once, seize them.
+      <li>Life is about the people you meet and the things you create with them, so go out and start creating.
+      <li>Life is short.
+      <li>Live your dream and share your passion.
+    </ul>
+  </li>
+  <li>
+    It's always good to be wrong, it shows you that you are learning something.
+  </li>
 </ul>
 
-<style>
-  li{font-style:italic;}
-</style>
