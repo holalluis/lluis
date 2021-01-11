705a91f37100b78d8233dfb96f12021c089d0177
Lluís Bosch <holalluis@gmail.com>
2021-01-11 11:38:05 +0100

    update

--- about.php
+++ about.php
@@ -6,8 +6,10 @@
 </p>
 <ul>
   <li>Treballo al centre de recerca <a href=//github.com/icra target=_blank>ICRA</a> (des del 2013).
-  <li>MSc en Computer Science     (<a href=//udg.edu target=_blank>UdG</a> 2012).
+  <li>Màster en Tecnologies de la Informació (<a href=//udg.edu target=_blank>UdG</a> 2012).
   <li>Llicenciat en Biotecnologia (<a href=//uab.cat target=_blank>UAB</a> 2011).
+  <li>Visc a Anglès (Girona, Catalunya).
+  <li>Vaig néixer el 1989.
 </ul>
 
 <b>Links</b>
