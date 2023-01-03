17b4bdb0baafaa9b5c739e69b17a664e5e641537
Lluís Bosch <holalluis@gmail.com>
2023-01-03 15:49:40 +0100

    update

--- turfwar.php
+++ turfwar.php
@@ -1,4 +1,8 @@
 <script>
+  fetch('https://ipv4.games/user.html?name=holalluis')
+    .then(response=>response.text())
+    .then(data=>console.log(data));
+
   fetch('http://ipv4.games/user.html?name=holalluis')
     .then(response=>response.text())
     .then(data=>console.log(data));
