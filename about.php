86bed6daec2ccf49641ec66c7367f22059649557
Lluís Bosch <holalluis@gmail.com>
2024-05-05 15:19:44 +0200

    update

--- about.php
+++ about.php
@@ -1,15 +1,15 @@
 <?php include'header.php'?>
 <b>Sobre mí</b>
 <p>
-  Benvingut a la meva pàgina personal: aquí hi trobaràs articles sobre Linux,
+  Benvingut/da a la meva pàgina personal: aquí hi trobaràs articles sobre Linux,
   software lliure, matemàtiques, física, química i biologia.
 </p>
 <ul>
   <li>Treballo al centre de recerca <a href=//github.com/icra target=_blank>ICRA</a> (des del 2013).
   <li>Màster en Tecnologies de la Informació (<a href=//udg.edu target=_blank>UdG</a> 2012).
   <li>Llicenciat en Biotecnologia (<a href=//uab.cat target=_blank>UAB</a> 2011).
-  <li>Visc a Anglès (Girona, Catalunya).
-  <li>Vaig néixer el 1989.
+  <li>Sóc d'Anglès (Girona), però visc a Torroella de Montgrí (Baix Empordà).
+  <li>Vaig néixer a Girona el 1989.
 </ul>
 
 <b>Links</b>
@@ -17,5 +17,5 @@
   <li><a href="//github.com/holalluis">github</a>
   <li><a href="mail.php">mail</a>
   <li><a href="//www.linkedin.com/in/holalluis">linkedin</a>
-  <li><a href="//www.twitter.com/lluisma">twitter</a>
+  <li><a href="//www.twitter.com/holalluis">twitter</a>
 </ul>
---
+++ blog/2024-05-05_-_ambigüitat.php
@@ -0,0 +1,25 @@
+<?php include'css.php'?>
+<h1>Sobre l'ambigüitat</h1><hr>
+<div><code>2024-05-05</code><div><hr><article>
+
+<p>
+L'ambigüitat és una característica que fa que el llenguatge sigui perillós. Els
+dobles sentits poden causar molt més dany que qualsevol arma.
+</p>
+
+<p>
+El que m'agrada del codi és que sempre diu la veritat. El codi executable és la
+cosa més honesta, virtuosa i democràtica que mai ha inventat la humanitat. Vols
+saber si un codi fa alguna cosa útil? Executa'l i mira si funciona.
+</p>
+
+<p>
+No pots fer això amb el català, l'anglès, el castellà... on tot el que importa
+és el que la gent creu.
+</p>
+
+<p>
+Fins i tot les matemàtiques no són tan bones: per exemple, les equacions LaTeX
+no són executables i, per tant, requereixen molt més esforç per saber si diuen
+alguna cosa útil.
+</p>
